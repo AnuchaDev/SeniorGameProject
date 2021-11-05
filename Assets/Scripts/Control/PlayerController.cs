a62b20da9e09a8a5b564060eccec87e1491a8652
AnuchaDev <anuchadev@gmail.com>
2021-11-05 17:13:33 +0700

    Let's begin

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using RPG.Combat;
+using RPG.Movement;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if(InteractWithCombat()) return;
+            InteractWithMovement();
+            print("Nothing to do.");
+        }
+
+        private bool InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private void InteractWithMovement()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+
+}
