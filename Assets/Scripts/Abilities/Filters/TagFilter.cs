bbe195feddf53cd152aa126edda832e99e8688b0
AnuchaDev <anuchadev@gmail.com>
2022-01-28 13:59:03 +0700

    Filter Strategy Pattern

---
+++ Assets/Scripts/Abilities/Filters/TagFilter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RPG.Abilities.Filters
+{
+    [CreateAssetMenu(fileName = "TagFilter", menuName = "Abilities/Filters/Tag", order = 0)]
+    public class TagFilter : FilterStrategy
+    {
+        [SerializeField] string tagToFilter = "";
+
+        public override IEnumerable<GameObject> Filter(IEnumerable<GameObject> objectsToFilter)
+        {
+            foreach(var gameObject in objectsToFilter)
+            {
+                if(gameObject.CompareTag(tagToFilter))
+                {
+                    yield return gameObject;
+                }
+            }
+        }
+    }
+}
